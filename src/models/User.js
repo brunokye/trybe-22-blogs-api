93f4c29633649f87866378008023575ca8baa0ce
Bruno Ferreira <contatobrunokye@gmail.com>
2023-04-06 17:03:04 -0300

    feat: requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      field: 'display_name',
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+
+  return User;
+};
